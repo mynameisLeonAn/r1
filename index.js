ca7166b5091246a9c98bbef31fb4d054f5306582
Wenli.lin <wenli.lin@echannelopen.com.tw>
2018-05-17 10:54:45 +0800

    fix Heroku

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 
 //=========  heroku  ========
 const PORT = process.env.PORT || 5000;
-//...
-server.listen(PORT);
-//==================================
-
-
-const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 const server = http.createServer((req, res) => {
@@ -14,6 +8,18 @@ const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World\n');
 });
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+server.listen(PORT);
+//==================================
+
+
+// const http = require('http');
+// const hostname = '127.0.0.1';
+// const port = 3000;
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World\n');
+// });
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
