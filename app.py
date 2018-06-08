03ec729b1b8579f789b6ca183d0f2779415ff75c
Wenli.lin <wenli.lin@echannelopen.com.tw>
2018-06-08 16:54:08 +0800

    add  爬蟲

--- app.py
+++ app.py
@@ -10,6 +10,11 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 from datetime import datetime
+import re
+import json
+from selenium import webdriver
+from bs4 import BeautifulSoup
+
 
 app = Flask(__name__)
 
@@ -93,6 +98,53 @@ def switch(x):
         3 :">>>>今天吃拿坡里",
         4 :">>>>今天吃八方",
     }.get(x,">>>>今天吃XXX")
+
+def notification(title, link):
+    with open('data/notify_list.json', 'r') as file:
+        notify_list = json.load(file)
+    if len(notify_list) == 0:
+        return False
+    
+    content = "{}\n{}".format(title, link)
+    line_bot_api.multicast(notify_list, TextSendMessage(text=content))
+    return True
+
+driver = webdriver.PhantomJS()
+driver.get('https://www.ptt.cc/bbs/Gamesale/index.html')
+soup = BeautifulSoup(driver.page_source, "html.parser")
+re_gs_title = re.compile(r'\[PS4\s*\]\s*售.*pro.*', re.I)
+re_gs_id = re.compile(r'.*\/Gamesale\/M\.(\S+)\.html')
+
+match = []
+for article in soup.select('.r-list-container .r-ent .title a'):
+    title = article.string
+    if re_gs_title.match(title) != None:
+        link = 'https://www.ptt.cc' + article.get('href')
+        article_id = re_gs_id.match(link).group(1)
+        match.append({'title':title, 'link':link, 'id':article_id})
+
+if len(match) > 0:
+    with open('data/history/gamesale.json', 'r+') as file:
+        history = json.load(file)
+
+        now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')       
+        new_flag = False
+        for article in match:
+            if article['id'] in history:
+                continue
+            new_flag = True
+            history.append(article['id'])
+            notification(article['title'], article['link'])
+            print("{}: New Article: {} {}".format(now, article['title'], article['link']))
+
+        if new_flag == True:
+            file.seek(0)
+            file.truncate()
+            file.write(json.dumps(history))
+        else:
+            print("{}: Nothing".format(now))
+
+
 
 import os
 if __name__ == "__main__":
