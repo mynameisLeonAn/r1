3b26357fd1155ff6650e429669e011ccadc9eaeb
Wenli.lin <wenli.lin@echannelopen.com.tw>
2018-06-14 19:00:14 +0800

    fix

--- app.py
+++ app.py
@@ -14,11 +14,11 @@ import re
 import json
 # import twitter
 from datetime import datetime
-# from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup
 
-# from apscheduler.schedulers.blocking import BlockingScheduler
-# from selenium import webdriver
-# from selenium.webdriver.chrome.options import Options
+from apscheduler.schedulers.blocking import BlockingScheduler
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 app = Flask(__name__)
@@ -117,55 +117,55 @@ def notification(title, link):
     return True
 
 
-# def scheduled_job():
-#     print("Action scheduled_job")
-#     # Chrome
-#     options = Options()
-#     options.binary_location = '/app/.apt/usr/bin/google-chrome'
-#     options.add_argument('--headless')
-#     options.add_argument('--disable-gpu')
-#     options.add_argument('--no-sandbox')
-#     driver = webdriver.Chrome(chrome_options=options)
-#     driver.implicitly_wait(10)
-#     driver.set_page_load_timeout(60)
+def scheduled_job():
+    print("Action scheduled_job")
+    # Chrome
+    options = Options()
+    options.binary_location = '/app/.apt/usr/bin/google-chrome'
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--no-sandbox')
+    driver = webdriver.Chrome(chrome_options=options)
+    driver.implicitly_wait(10)
+    driver.set_page_load_timeout(60)
 
 
-#     driver.get('https://www.ptt.cc/bbs/Gamesale/index.html')
-#     soup = BeautifulSoup(driver.page_source, "html.parser")
-#     re_gs_title = re.compile(r'\[PS4\s*\]\s*售.*pro.*', re.I)
-#     re_gs_id = re.compile(r'.*\/Gamesale\/M\.(\S+)\.html')
+    driver.get('https://www.ptt.cc/bbs/Gamesale/index.html')
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+    re_gs_title = re.compile(r'\[PS4\s*\]\s*售.*pro.*', re.I)
+    re_gs_id = re.compile(r'.*\/Gamesale\/M\.(\S+)\.html')
 
-#     print("re_gs_title="+re_gs_title)
-#     print("re_gs_id="+re_gs_id)
+    print("re_gs_title="+re_gs_title)
+    print("re_gs_id="+re_gs_id)
 
-#     match = []
-#     for article in soup.select('.r-list-container .r-ent .title a'):
-#         title = article.string
-#         if re_gs_title.match(title) != None:
-#             link = 'https://www.ptt.cc' + article.get('href')
-#             article_id = re_gs_id.match(link).group(1)
-#             match.append({'title':title, 'link':link, 'id':article_id})
+    match = []
+    for article in soup.select('.r-list-container .r-ent .title a'):
+        title = article.string
+        if re_gs_title.match(title) != None:
+            link = 'https://www.ptt.cc' + article.get('href')
+            article_id = re_gs_id.match(link).group(1)
+            match.append({'title':title, 'link':link, 'id':article_id})
 
-#     if len(match) > 0:
-#         print("match="+match)
-#         with open('data/history/gamesale.json', 'r+') as file:
-#             history = json.load(file)
-#             now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')       
-#             new_flag = False
-#             for article in match:
-#                 if article['id'] in history:
-#                     continue
-#                 new_flag = True
-#                 history.append(article['id'])
-#                 notification(article['title'], article['link'])
-#                 print("{}: New Article: {} {}".format(now, article['title'], article['link']))
+    if len(match) > 0:
+        print("match="+match)
+        with open('data/history/gamesale.json', 'r+') as file:
+            history = json.load(file)
+            now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')       
+            new_flag = False
+            for article in match:
+                if article['id'] in history:
+                    continue
+                new_flag = True
+                history.append(article['id'])
+                notification(article['title'], article['link'])
+                print("{}: New Article: {} {}".format(now, article['title'], article['link']))
 
-#             if new_flag == True:
-#                 file.seek(0)
-#                 file.truncate()
-#                 file.write(json.dumps(history))
-#             else:
-#                 print("{}: Nothing".format(now))
+            if new_flag == True:
+                file.seek(0)
+                file.truncate()
+                file.write(json.dumps(history))
+            else:
+                print("{}: Nothing".format(now))
 
 
 
@@ -174,7 +174,7 @@ if __name__ == "__main__":
     app.run(host='0.0.0.0',port=os.environ['PORT'])
 
 
-# # Job
-# sched = BlockingScheduler()
-# sched.add_job(func=scheduled_job, trigger='cron', second='*/3000')
-# sched.start()
+# Job
+sched = BlockingScheduler()
+sched.add_job(func=scheduled_job, trigger='cron', second='*/3000')
+sched.start()
