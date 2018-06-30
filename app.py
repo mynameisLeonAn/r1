91cd6ff3f2d68324c42e8e661062e3640442ac6f
Wenli.lin_PC <jusrnow091@gmail.com>
2018-06-30 20:10:33 +0800

    fix help :ButtonsTemplate

--- app.py
+++ app.py
@@ -158,17 +158,23 @@ def helpMessage(event):
     # )
     button_template_message =ButtonsTemplate(
         thumbnail_image_url='https://78.media.tumblr.com/82890f75107edef4fb5b4a4af6c2cd40/tumblr_oxq1209UsI1uzwbyjo1_540.gif',
-        title='LIN_BOT功能',
-        text=shelpMessage.format(sToolName1, sToolName2, sToolName3),
+        title='Menu', 
+        text='Please select',
         image_size="cover",
         actions=[
-            MessageTemplateAction(
-                    label='想吃or要吃 :隨機垃圾食物',
-                    text='今天要吃什麼'
+            #   PostbackTemplateAction 點擊選項後，
+            #   除了文字會顯示在聊天室中，
+            #   還回傳data中的資料，可
+            #   此類透過 Postback event 處理。
+            PostbackTemplateAction(
+                label='查詢個人檔案顯示文字-Postback', 
+                text='查詢個人檔案',
+                data='action=buy&itemid=1'
             ),
-            MessageTemplateAction(
-                label='找PTT :TypeMoon>日GO',
-                text='找PTT :TypeMoon>日GO'
+            PostbackTemplateAction(
+                label='不顯示文字-Postback', 
+                text = None,
+                data='action=buy&itemid=1'
             ),
             MessageTemplateAction(
                 label='找推特圖 FGO',
