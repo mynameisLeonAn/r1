f53a11b4157c96209c9f890e1fdd218d5132a911
Wenli.lin <wenli.lin@echannelopen.com.tw>
2018-08-17 17:30:43 +0800

    scheduled_job

--- util/main_test.py
+++ util/main_test.py
@@ -3,10 +3,17 @@ import datetime
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 from flindUtil import ptt_beauty,ptt_gossiping,ptt_AC_In,ptt_find,findPTT
+from flask import Flask, request, abort
 
+
+app = Flask(__name__)
+@app.route("/")
+def hello():
+    now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')       
+    return "Hello !:"+now
 
 sched = BlockingScheduler()
-@sched.scheduled_job('interval', minutes=20) #定期執行，每X分鐘執行一次
+@sched.scheduled_job('interval', minutes=0.2) #定期執行，每X分鐘執行一次
 def job_GBF():
     print('Start job_GBF') #運行時打印出此行訊息
     sReturn = ptt_find("GBF")
@@ -16,9 +23,9 @@ def job_GBF():
         now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')  
         sReturn = "{}--查無結果".format(now)
 
-    print('END job_GBF:')#運行時打印出此行訊息
+    print('END job_GBF:'+sReturn)#運行時打印出此行訊息
 
-@sched.scheduled_job('interval', minutes=20) #定期執行，每X分鐘執行一次
+@sched.scheduled_job('interval', minutes=0.1) #定期執行，每X分鐘執行一次
 def job_TypeMoon():
     print('Start job_TypeMoon') #運行時打印出此行訊息
     sReturn = ptt_find("TypeMoon")
@@ -28,6 +35,10 @@ def job_TypeMoon():
         now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')  
         sReturn = "{}--查無結果".format(now)
 
-    print('END job_TypeMoon:')#運行時打印出此行訊息
+    print('END job_TypeMoon:'+sReturn)#運行時打印出此行訊息
  
 sched.start()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=os.environ['PORT'])
